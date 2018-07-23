678016f40dc570e4ca139b07b585535fbec35d17
Burkey Koontz <bk.koontz@gmail.com>
2018-07-23 09:04:18 -0700

    changed document ready function

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,16 @@
 // eslint-disable-next-line no-unused-vars
 
 $(document).ready(function() {
+  const itemNames = [ '', 'apples', 'pears' ];
+  itemNames.forEach(name => {
+  try {
+    Item.validateName(name);
+    store.items.push(Item.create(name));
+  } catch(error) {
+    console.log('Cannot add item: ' + error.message);
+  }
+  });
   shoppingList.bindEventListeners();
   shoppingList.render();
+
 });
